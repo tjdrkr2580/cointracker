2a98c709b08712447dc7748aed6a72e89733c866
Kim Tae Hyun <tjdrkr2580@naver.com>
2022-08-30 01:54:39 +0900

    추가 styled-components, fetch

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,18 @@
+import { useEffect, useState } from "react";
+import styled, { ThemeProvider } from "styled-components";
+
 function App() {
+  const [Loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers?quotes=KRW").then(
+      (response) =>
+        response.json().then((json) => {
+          setCoins(json.slice(0, 100));
+          setLoading(false);
+        })
+    );
+  }, []);
   return <div></div>;
 }
 
